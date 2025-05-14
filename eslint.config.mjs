5573bd52114cf476f535358ad4bca4a7e01e0e67
EduardoJvn <ejvn30@gmail.com>
2025-05-14 16:33:47 -0300

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,9 +26,15 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn', // Advertencia cuando se usa `any`
+      '@typescript-eslint/no-unused-vars': 'warn', // Advertencia por variables no usadas
+      '@typescript-eslint/explicit-module-boundary-types': 'off', // Desactivar la regla para exigir tipos explícitos en funciones de módulos
+      // 'import/no-unresolved': 'error', // Errores cuando no se puede resolver un módulo importado
+      // 'import/no-named-as-default': 'off', // Desactiva una regla para mejorar la compatibilidad con ciertos imports
+      'no-console': 'warn', // Advertencia para evitar el uso de `console.log`
+      'no-unused-vars': 'off', // Desactiva la regla de ESLint para variables no usadas (porque la maneja `@typescript-eslint/no-unused-vars`)
     },
   },
 );
